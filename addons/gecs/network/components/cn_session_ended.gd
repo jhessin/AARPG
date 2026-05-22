e53e2f82289c14379613a7661775e85d64bff43d
Jim Hessin <jhessin@gmail.com>
2026-05-21 20:03:52 -0600

    Removed gecs

--- addons/gecs/network/components/cn_session_ended.gd
+++
@@ -1,4 +0,0 @@
-class_name CN_SessionEnded
-extends Component
-## Transient event component: emitted when the multiplayer session ends.
-## Added to the session entity for one frame, then cleared by NetworkSession.
--- addons/gecs/network/components/cn_session_state.gd
+++
@@ -1,8 +0,0 @@
-class_name CN_SessionState
-extends Component
-## Permanent state component: tracks live connection state for the session entity.
-## Kept on the session entity throughout the session lifetime.
-
-@export var is_connected: bool = false
-@export var is_hosting: bool = false
-@export var peer_count: int = 0
