2b1088f9faac9966bc236e5b0b9a45ba693ae0e8
Jim Hessin <jhessin@gmail.com>
2026-05-24 11:39:20 -0600

    Refactored and commented CombatSystem.py

--- ECS/Systems/CombatSystem.py
+++ ECS/Systems/CombatSystem.py
@@ -1,17 +1,19 @@
 import esper
 from py4godot.classes.Area2D import Area2D
+from py4godot.classes.Area2DTypedArray import Area2DTypedArray
 from ..components import (
     ENTITY_ID,
     BodyComponent,
     HitboxComponent,
-    # HurtboxComponent,
     HealthComponent,
+    HurtboxComponent,
     VelocityComponent,
 )
 
 
 class CombatSystem(esper.Processor):
     def process(self, _delta: float) -> None:
+        # Not using delta so we can safely delete it for now.
         del _delta
 
         # Loop through any entities with active attacking weapon components this requires a hitbox and a body
@@ -20,14 +22,13 @@ class CombatSystem(esper.Processor):
         ):
             # Access the node tracking property saved inside the component data
             hitbox_node: Area2D = Area2D.cast(hitbox.node)
-            if not attacker_body.body or attacker_body.body.is_queued_for_deletion():
-                continue
 
+            # Check if we have already hit something or we are not attacking
             if not hitbox_node or not hitbox_node.is_monitoring():
                 continue
 
             # Query overlapping vectors natively
-            overlapping_areas = hitbox_node.get_overlapping_areas()
+            overlapping_areas: Area2DTypedArray = hitbox_node.get_overlapping_areas()
 
             # Loop through each area that was hit.
             for i in range(overlapping_areas.size()):
@@ -36,8 +37,13 @@ class CombatSystem(esper.Processor):
                 # Check for a hurtbox component
                 if victim_area_node.has_meta(ENTITY_ID):
                     victim_ent = int(str(victim_area_node.get_meta(ENTITY_ID)))
-                    # hurtbox_node = Area2D.cast(overlapping_areas.get(i))
 
+                    # If our victim doesn't have a hurtbox these aren't the
+                    # droids we are looking for 😁.
+                    if not esper.has_component(victim_ent, HurtboxComponent):
+                        continue
+
+                    # Do not attack yourself
                     if attacker_ent == victim_ent:
                         continue
 
@@ -46,21 +52,15 @@ class CombatSystem(esper.Processor):
                         victim_ent, HealthComponent
                     ):
                         # process health stuff here
-                        if victim_health.current <= 0:
-                            continue
                         victim_health.current -= hitbox.damage
-                        print(
-                            f"Entity #{victim_ent} damaged by #{attacker_ent}! HP: {victim_health.current}/{victim_health.maximum}"
-                        )
 
-                    # Apply knockback direction vectors
+                    # Apply knockback direction if the victim has a body and
+                    # VelocityComponent
                     if (
                         victim_body := esper.try_component(victim_ent, BodyComponent)
                     ) and (
                         victim_vel := esper.try_component(victim_ent, VelocityComponent)
                     ):
-                        if victim_body.body.is_queued_for_deletion():
-                            continue
                         diff = (
                             victim_body.body.global_position
                             - attacker_body.body.global_position
@@ -70,27 +70,5 @@ class CombatSystem(esper.Processor):
                         victim_vel.x = dir_vector.x * hitbox.knockback_force
                         victim_vel.y = dir_vector.y * hitbox.knockback_force
 
+                    # Disable the hitbox node until the next attack
                     hitbox_node.call_deferred("set_monitoring", False)
-
-        # # --- ENGINE PROBE TRACKER ---
-        # raw_loop = Engine.instance().get_main_loop()
-        # if raw_loop:
-        #     # Cast the generic MainLoop up to its true running type: SceneTree
-        #     scene_tree = SceneTree.cast(raw_loop)
-        #     player_nodes = scene_tree.get_nodes_in_group("Player")
-        #
-        #     for i in range(player_nodes.size()):
-        #         player_node = player_nodes.get(i)
-        #         if hitbox_node := player_node.get_node("HitBox"):
-        #             hitbox_area = Area2D.cast(hitbox_node)
-        #
-        #             # Print properties directly to pinpoint the failure
-        #             print(
-        #                 f"[DEBUG] Hitbox Monitoring: {hitbox_area.is_monitoring()} | Mask: {hitbox_area.get_collision_mask()}"
-        #             )
-        #
-        #             overlaps = hitbox_area.get_overlapping_areas()
-        #             print(
-        #                 f"[DEBUG] Active Area Overlaps detected by Godot: {overlaps.size()}"
-        #             )
-        # # --- ENGINE PROBE TRACKER ---
